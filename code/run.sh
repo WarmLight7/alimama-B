32e7554c6468d47caa5d4b724036a0cda1bf3d02
gansongyuan <gansongyuan@nowcoder.com>
2023-06-25 19:23:05 +0800

    update imagename

--- code/run.sh
+++ code/run.sh
@@ -2,4 +2,4 @@
 
 # ./alimama/demo_alimama_search_service --data_file /data/out.csv
 # ./src/server --data_file /data/out70g.csv
-./src/server --data_file /data/out.csv
+./src/server --data_file /data/data.csv
