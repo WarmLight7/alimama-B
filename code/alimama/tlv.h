3a91f08087b004f1e1f1938251ad1bd264deb714
konglingdi <konglingdii@163.com>
2023-07-04 22:25:30 +0800

    tlv跨机通信第二版

--- code/alimama/tlv.h
+++ code/alimama/tlv.h
@@ -1,55 +1,163 @@
 #include <iostream>
-#include <cstd::string>
+#include <string>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <vector>
 
-// TLV结构体
-class TLV {
-private:
-    uint8_t type;
-    uint16_t length;
-    char* value = nullptr;
-public:
-    TLV(uint8_t type_, uint16_t length_, std::string value_)
+// RPC请求与响应编码
+struct rpcRequest
+{
+    std::vector<uint64_t> keywords;
+    float context_vector[2];
+    uint64_t hour;
+    uint64_t topn;
+
+    void print()
     {
-        type = type_; // 0发送，1接收
-        length = length_;
-        value_ = new char[length];
-        std::memcpy(value, value_.c_str(), length);
-    }
-    void encoder(char* buffer) // 将TLV结构体编码成字节流
-    {
-        buffer[0] = type;
-        buffer[1] = length >> 8;  // 长度高字节
-        buffer[2] = length & 0xFF; // 长度低字节
-        std::memcpy(buffer + 3, value, length);
-    }
-    void decoder(const char* buffer) // 解析TLV字节流为TLV结构体
-    {
-        type = buffer[0];
-        length = (buffer[1] << 8) | buffer[2];
-        if(value) delete[] value;
-        value = new char[length];
-        std::memcpy(value, buffer + 3, length);
-    }
-    ~TLV()
-    {
-        if(value) delete[] value;
+        std::cout << "  keywords: ";
+        for(auto& keyword:keywords)
+        {
+            std::cout<<keyword<<" ";
+        }
+        std::cout << std::endl;
+        std::cout << "  context_vector: " << context_vector[0] << " " << context_vector[1] << std::endl;
+        std::cout << "  hour: " << hour << std::endl;
+        std::cout << "  topn: " << topn << std::endl;
+        std::cout<<std::endl;
     }
 };
+struct rpcResponse
+{
+    std::vector<uint64_t> adgroup_ids;
+    std::vector<uint64_t> prices;
+
+    void print()
+    {
+        std::cout << "  adgroup_ids: ";
+        for(auto& adgroup_id:adgroup_ids)
+        {
+            std::cout<<adgroup_id<<" ";
+        }
+        std::cout<<std::endl;
+        std::cout << "  prices: ";
+        for(auto& price:prices)
+        {
+            std::cout<<price<<" ";
+        }
+        std::cout<<std::endl;
+        std::cout<<std::endl;
+    }
+};
+void RPCRequest2Bytes(rpcRequest& req, char* buffer)
+{
+    buffer[0] = 0; // TLV 类型 0：RPC请求 1：RPC响应
+    //编码长度
+
+    int index = 3; // 1,2字节存储长度
+    std::memcpy(buffer + index, reinterpret_cast<char*>(&req.hour), sizeof(uint64_t));
+
+    index += sizeof(uint64_t);
+    std::memcpy(buffer + index, reinterpret_cast<char*>(&req.topn), sizeof(uint64_t));
+
+    index += sizeof(uint64_t);
+    std::memcpy(buffer + index, reinterpret_cast<char*>(&req.context_vector[0]), sizeof(float));
+
+    index += sizeof(float);
+    std::memcpy(buffer + index, reinterpret_cast<char*>(&req.context_vector[1]), sizeof(float));
+
+    index += sizeof(float);
+    for(int j = 0; j < req.keywords.size(); j++) 
+    {
+        std::memcpy(buffer + index, reinterpret_cast<char*>(&req.keywords[j]), sizeof(uint64_t));
+        index += sizeof(uint64_t);
+    }
+
+    // TLV 存储长度
+    buffer[1] = index >> 8;  // 长度高字节
+    buffer[2] = index & 0xFF; // 长度低字节
+}
+void Bytes2RPCRequest(rpcRequest& req, const char* buffer) 
+{
+    // type = buffer[0];
+    int length = (buffer[1] << 8) | buffer[2];
+
+    int index = 3;
+    std::memcpy(&req.hour, &buffer[index], sizeof(uint64_t));
+
+    index += sizeof(uint64_t);
+    std::memcpy(&req.topn, &buffer[index], sizeof(uint64_t));
+
+    index += sizeof(uint64_t); // 在这个地方开始copy
+    for(int j = 0; j < 2; j++) 
+    {
+        std::memcpy(&req.context_vector[j], &buffer[index], sizeof(float));
+        index += sizeof(float);
+    }
+    // 解析keyword数组
+    for(; index < length; index+=sizeof(uint64_t)) 
+    {
+        uint64_t temp = 0;
+        std::memcpy(&temp, &buffer[index], sizeof(uint64_t));
+        req.keywords.push_back(temp);
+    }
+}
+void RPCResponse2Bytes(rpcResponse& res, char* buffer)
+{
+    buffer[0] = 1; // // TLV 类型 0：RPC请求 1：RPC响应
+    //编码长度
+
+    int index = 3; // 1,2字节存储长度
+    for(int j = 0; j < res.adgroup_ids.size(); j++) 
+    {
+        std::memcpy(buffer + index, reinterpret_cast<char*>(&res.adgroup_ids[j]), sizeof(uint64_t));
+        index += sizeof(uint64_t);
+    }
+    for(int j = 0; j < res.prices.size(); j++) 
+    {
+        std::memcpy(buffer + index, reinterpret_cast<char*>(&res.prices[j]), sizeof(uint64_t));
+        index += sizeof(uint64_t);
+    }
+
+    // TLV 存储长度
+    buffer[1] = index >> 8;  // 长度高字节
+    buffer[2] = index & 0xFF; // 长度低字节
+}
+void Bytes2RPCResponse(rpcResponse& res, const char* buffer) 
+{
+    // type = buffer[0];
+    int length = (buffer[1] << 8) | buffer[2];
+
+    int index = 3;
+
+    // 解析adgroup_ids数组
+    for(; index < 3 + (length - 3) / 2; index += sizeof(uint64_t)) 
+    {
+        uint64_t temp = 0;
+        std::memcpy(&temp, &buffer[index], sizeof(uint64_t));
+        res.adgroup_ids.push_back(temp);
+    }
+
+    // 解析prices数组
+    for(; index < length; index+=sizeof(uint64_t)) 
+    {
+        uint64_t temp = 0;
+        std::memcpy(&temp, &buffer[index], sizeof(uint64_t));
+        res.prices.push_back(temp);
+    }
+}
 
 class TLVServer {
 private:
     int sockfd;
     struct sockaddr_in serverAddr, clientAddr;
     socklen_t addrLen;
-    char buffer[1024];
+    char buffer[1024]; // UDP数据帧最大载荷
 
     int _port;
 public:
-    TLVServer(port = 12345)
+    TLVServer(int port = 12345)
     {
         _port = port;
 
@@ -58,7 +166,7 @@ public:
         sockfd = socket(AF_INET, SOCK_DGRAM, 0);
         if (sockfd < 0) {
             std::cerr << "Failed to create socket." << std::endl;
-            return 1;
+            return;
         }
 
         // 设置服务器地址和端口
@@ -69,10 +177,10 @@ public:
         // 将套接字绑定到服务器地址
         if (bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
             std::cerr << "Failed to bind socket." << std::endl;
-            return 1;
+            return;
         }
     }
-    TLVListening()
+    int TLVListening()
     {
         while (true) {
             // 接收数据
@@ -82,19 +190,29 @@ public:
                 std::cerr << "Failed to receive data." << std::endl;
                 return 1;
             }
-
-            // 解析收到的TLV数据
-            TLV receivedTLV;
-            decodeTLV(buffer, receivedTLV);
-
-            // 打印收到的TLV内容
-            std::cout << "Received TLV:" << std::endl;
-            std::cout << "Type: " << (int)receivedTLV.type << std::endl;
-            std::cout << "Length: " << receivedTLV.length << std::endl;
-            std::cout << "Value: " << receivedTLV.value << std::endl;
-
-            // 释放内存
-            delete[] receivedTLV.value;
+            // 解析字节流的TLV数据
+            if( (int)buffer[0] == 1) //rpc响应
+            {
+                rpcResponse res;
+                Bytes2RPCResponse(res,buffer);
+                // 打印收到的TLV内容
+                std::cout << "Received TLV:" << std::endl;
+                std::cout << "Type: " << (int)buffer[0] << std::endl;
+                std::cout << "Length: " << (int)((buffer[1] << 8) | buffer[2]) << std::endl;
+                std::cout << "Value: " << std::endl;
+                res.print();
+            }
+            else //rpc请求
+            {
+                rpcRequest req;
+                Bytes2RPCRequest(req, buffer);
+                // 打印收到的TLV内容
+                std::cout << "Received TLV:" << std::endl;
+                std::cout << "Type: " << (int)buffer[0] << std::endl;
+                std::cout << "Length: " << (int)((buffer[1] << 8) | buffer[2]) << std::endl;
+                std::cout << "Value: " << std::endl;
+                req.print();
+            }
         }
     }
     ~TLVServer()
@@ -111,8 +229,8 @@ private:
     std::string _hostname;
     int _port;
 public:
-    TLVClient(const std::string& hostname, port = 12345)
-    {        
+    TLVClient(const std::string& hostname, int port = 12345)
+    {
         _hostname = hostname;
         _port = port;
 
@@ -136,10 +254,10 @@ public:
             return;
         }        
     }
-    TLVSendMessage(const std::string& message)
+    void TLVSendMessage(char* buffer, int length)
     {
         // 发送消息
-        ssize_t numBytesSent = sendto(sockfd, message.c_str(), message.length(), 0, res->ai_addr, res->ai_addrlen);
+        ssize_t numBytesSent = sendto(sockfd, buffer, length, 0, res->ai_addr, res->ai_addrlen);
         if (numBytesSent == -1) {
             std::cerr << "sendto error" << std::endl;
             close(sockfd);
@@ -148,7 +266,7 @@ public:
         }
         std::cout << "Sent " << numBytesSent << " bytes to " << _hostname << ":" << _port << std::endl;
     }
-    ~TLVServer()
+    ~TLVClient()
     {
         close(sockfd);
         freeaddrinfo(res);
