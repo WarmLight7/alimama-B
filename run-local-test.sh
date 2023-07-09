9ef07e4edd329e4b5e7022b2d234ac967bc313e9
WarmLight7 <1322872313@qq.com>
2023-07-10 01:16:47 +0800

    大版本更新

--- run-local-test.sh
+++ run-local-test.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 echo "0-Init"
-echo "$0"
 cd `dirname $0`
-rm node-1 node-2 node-3 build -rf || true
+rm -rf node-1 node-2 node-3 build || true
 
 echo "1-Tar"
 cd `dirname $0`
-rm code.tar.gz -rf || true
-#考生相关：指定这个code.tar.gz就是用于提交的代码压缩包
-tar -czf code.tar.gz --directory=code .
+rm -rf code.tar.gz || true
+docker run --rm -v /`pwd`/://work -w //work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v1 checktar code
 
-echo "2-Build"s
+
+echo "2-Build"
 cd `dirname $0`
-mkdir build
-tar -xzf code.tar.gz --director=build
+cp -rf code build
 #考生相关: build.sh是构建入口脚本, run.sh是运行入口脚本
-# docker run --rm  -v "D:\alimama\prob-alimama-2023-B/build:/work" -w /work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v0
-# docker run --rm  -v "D:\alimama\prob-alimama-2023-B/testbench:/work" -w /work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v0
-docker run --rm  -v "D:\alimama\prob-alimama-2023-B/build:/work" -w /work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v0 bash build.sh
-docker run --rm  -v "D:\alimama\prob-alimama-2023-B/testbench:/work" -w /work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v0 bash build.sh
+docker run --rm -v /`pwd`/build://work -w //work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v1 bash build.sh
+docker run --rm -v /`pwd`/testbench://work -w //work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v1 bash build.sh
 
 echo "3-Run"
 cd `dirname $0`
-cp build node-1 -rp
-cp build node-2 -rp
-cp build node-3 -rp
+cp -rp build node-1
+cp -rp build node-2
+cp -rp build node-3 
 docker compose down
 docker compose up -d
 docker compose logs -f testbench
--- tar-code-for-submit.sh
+++ tar-code-for-submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tar -czf code.tar.gz --directory=code .
+docker run --rm  -v /`pwd`/://work -w //work --network none public-images-registry.cn-hangzhou.cr.aliyuncs.com/public/alimama-2023:B-v1 checktar code
